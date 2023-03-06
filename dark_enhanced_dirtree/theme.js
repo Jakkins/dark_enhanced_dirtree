8909f6ce10846e44e02eb3345dc4ed6e25f76651
Jakkins <sjakkins@proton.me>
2023-03-06 18:17:31 +0100

    keeping track of last nav item clicked

--- dark_enhanced_dirtree/theme.js
+++ dark_enhanced_dirtree/theme.js
@@ -1,31 +1,42 @@
 (function () {
-  // ready ?
   console.log("ready");
 })();
 
-function handleCollapseToggle() {}
+let last_active_nav_el = null;
+
+function setActive(target) {
+  console.log("setActive", target);
+  if (last_active_nav_el !== null) {
+    last_active_nav_el.classList.toggle("active");
+  }
+  target.classList.toggle("active");
+  last_active_nav_el = target;
+}
 
 window.addEventListener("load", function (event) {
-  // after images and css
-  // let lis_el = document.querySelectorAll("li.section");
+  /**
+   * dirtree functionality === css + js
+   */
   let sections_nodelist = this.document.getElementsByClassName("section");
   for (var i = 0, len = sections_nodelist.length; i < len; i++) {
     sections_nodelist[i].addEventListener(
       "click",
       function (event) {
-        console.log(event);
         event.target.classList.toggle("collapsed");
       },
       false
     );
   }
 
+  /**
+   * change page without refresh functionality
+   */
   let pages_nodelist = this.document.getElementsByClassName("nav-page");
   for (var i = 0, len = pages_nodelist.length; i < len; i++) {
     pages_nodelist[i].addEventListener(
       "click",
       function (event) {
-        console.log(event);
+        setActive(event.target);
         let page_content = document.getElementById("page_content");
         page_content.innerHTML = "";
         var xhr = new XMLHttpRequest(); // create new XMLHttpRequest object
@@ -34,7 +45,7 @@ window.addEventListener("load", function (event) {
             let content = xhr.responseText.split(
               '<div id="page_content" class="content">'
             )[1];
-            content = content.split("<br id=\"parse-me-away\"/>")[0]
+            content = content.split('<br id="parse-me-away"/>')[0];
             document.getElementById("page_content").innerHTML = content; // update DOM with response
             hljs.highlightAll();
           }
