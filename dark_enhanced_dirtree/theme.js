a6f9ee40e42dd96d0518f554def6ec5db620475f
Jakkins <sjakkins@proton.me>
2023-03-07 10:49:05 +0100

    setNavActiveByUrl: click on link will uncollapse the nav

--- dark_enhanced_dirtree/theme.js
+++ dark_enhanced_dirtree/theme.js
@@ -2,7 +2,31 @@
   console.log("every page loaddd");
 })();
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 let last_active_nav_el = null;
+
+async function setNavActiveByUrl(url) {
+  let li_nodelist = this.document.getElementsByClassName("nav-page");
+  for (var i = 0, len = li_nodelist.length; i < len; i++) {
+    let a_el = li_nodelist[i].getElementsByTagName("a")[0]; // get a inside li
+    if (a_el.href === url) {
+      setActive(li_nodelist[i]);
+      const li_children = li_nodelist[i].closest("li.children");
+      let li_section = li_children.previousElementSibling;
+      while (li_section) {
+        if (li_section.classList.contains("section")) {
+          li_section.classList.remove("collapsed");
+          let li_children = li_section.closest("li.children");
+          li_section =
+            li_children !== null ? li_children.previousElementSibling : null;
+        }
+      }
+    }
+  }
+}
 
 function setAElements() {
   let all_a_el = document.getElementsByTagName("a");
@@ -12,9 +36,14 @@ function setAElements() {
       if (url_str.includes(window.location.host)) {
         let last_url_char = url_str[url_str.length - 1];
         if (last_url_char === "/") {
-          loadPage(url_str);
-          // TODO set active
-          e.preventDefault();
+          try {
+            loadPage(url_str);
+            setNavActiveByUrl(url_str);
+          } catch (e) {
+            console.log(e);
+          } finally {
+            e.preventDefault();
+          }
         } else {
           // filter mp4, mp3, etc.... do nothing
         }
@@ -26,7 +55,6 @@ function setAElements() {
 }
 
 function setActive(target) {
-  console.log("setActive", target);
   if (last_active_nav_el !== null) {
     last_active_nav_el.classList.toggle("active");
   }
