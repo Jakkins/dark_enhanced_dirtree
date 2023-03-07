158b69affc426628e254b4dc0af8ba7660bbc77a
Jakkins <sjakkins@proton.me>
2023-03-07 10:06:31 +0100

    support internal page requests

--- dark_enhanced_dirtree/theme.js
+++ dark_enhanced_dirtree/theme.js
@@ -1,8 +1,29 @@
 (function () {
-  console.log("ready");
+  console.log("every page loaddd");
 })();
 
 let last_active_nav_el = null;
+
+function setAElements() {
+  let all_a_el = document.getElementsByTagName("a");
+  for (var i = 0, len = all_a_el.length; i < len; i++) {
+    all_a_el[i].onclick = function (e) {
+      let url_str = e.target.href;
+      if (url_str.includes(window.location.host)) {
+        let last_url_char = url_str[url_str.length - 1];
+        if (last_url_char === "/") {
+          loadPage(url_str);
+          // TODO set active
+          e.preventDefault();
+        } else {
+          // filter mp4, mp3, etc.... do nothing
+        }
+      } else {
+        // external URL, do nothing (for now)
+      }
+    };
+  }
+}
 
 function setActive(target) {
   console.log("setActive", target);
@@ -13,7 +34,27 @@ function setActive(target) {
   last_active_nav_el = target;
 }
 
+function loadPage(url) {
+  let page_content = document.getElementById("page_content");
+  page_content.innerHTML = "";
+  var xhr = new XMLHttpRequest(); // create new XMLHttpRequest object
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let content = xhr.responseText.split(
+        '<div id="page_content" class="content">'
+      )[1];
+      content = content.split('<br id="parse-me-away"/>')[0];
+      document.getElementById("page_content").innerHTML = content; // update DOM with response
+      hljs.highlightAll();
+      setAElements();
+    }
+  };
+  xhr.open("GET", url, true); // open the request with the GET method
+  xhr.send(); // send the request
+}
+
 window.addEventListener("load", function (event) {
+  setAElements();
   /**
    * dirtree functionality === css + js
    */
@@ -37,22 +78,8 @@ window.addEventListener("load", function (event) {
       "click",
       function (event) {
         setActive(event.target);
-        let page_content = document.getElementById("page_content");
-        page_content.innerHTML = "";
-        var xhr = new XMLHttpRequest(); // create new XMLHttpRequest object
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4 && xhr.status === 200) {
-            let content = xhr.responseText.split(
-              '<div id="page_content" class="content">'
-            )[1];
-            content = content.split('<br id="parse-me-away"/>')[0];
-            document.getElementById("page_content").innerHTML = content; // update DOM with response
-            hljs.highlightAll();
-          }
-        };
         let a_el_page = event.target.firstElementChild; // HTMLAnchorElement
-        xhr.open("GET", a_el_page.pathname, true); // open the request with the GET method
-        xhr.send(); // send the request
+        loadPage(a_el_page.pathname);
       },
       false
     );
--- my_static_pages/docs/index.md
+++ my_static_pages/docs/index.md
@@ -1,1 +1,4 @@
 # jakkins
+
+[this won't work](./how%20to%20create%20a%20custom%20theme/1.%20enhanced/000.md)
+[google](https://www.google.com)
