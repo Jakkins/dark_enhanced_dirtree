86b8d259ee3287b4bd2d95283e2c4b352fc2dd19
Jakkins <sjakkins@proton.me>
2023-03-10 15:41:40 +0100

    v1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # https://pypi.org/project/mkdocs-dark-minimal-dirtree
 
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 
 setup(
     name="mkdocs-dark_enhanced_dirtree",
