04dbfe666abf15301333e4d98898f36be109ddc2
Jakkins <sjakkins@proton.me>
2023-03-10 15:03:44 +0100

    ssite script now in python

---
+++ my_static_pages/ssite.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from glob import glob
+import shutil
+import sys
+import os
+
+static_page_repo = "jakkins.github.io"
+
+
+def print_help():
+    print("args supported:")
+    print("- goprod")
+    print("- serve")
+    print()
+    print("example: site serve")
+    sys.exit(1)
+
+
+if len(sys.argv) == 1:
+    print_help()
+
+ABSPATH = os.path.realpath(os.path.dirname(__file__))
+
+if sys.argv[1] == "serve":
+    os.chdir(ABSPATH)
+    os.system("python -m mkdocs serve")
+
+if sys.argv[1] == "goprod":
+    os.chdir(ABSPATH)
+    os.system("python -m mkdocs build")
+    os.chdir(f"../../{static_page_repo}")
+    files = glob("*")
+    for file in files:
+        if file not in [".git", "readme.md"]:
+            if os.path.isdir(file):
+                shutil.rmtree(file)
+            else:
+                os.remove(file)
+    src_dir = "../dark_enhanced_dirtree/my_static_pages/site/"
+    dst_dir = "./"
+    for dirpath, dirnames, filenames in os.walk(src_dir):
+        # Iterate over the directories and move each one to the destination directory
+        for dirname in dirnames:
+            src_path = os.path.join(dirpath, dirname)
+            dst_path = src_path.replace(src_dir, dst_dir, 1)
+            shutil.move(src_path, dst_path)
+        # Iterate over the files and move each one to the destination directory
+        for filename in filenames:
+            src_path = os.path.join(dirpath, filename)
+            dst_path = src_path.replace(src_dir, dst_dir, 1)
+            shutil.move(src_path, dst_path)
+    os.system("git add .")
+    os.system('git commit -m "goprod"')
+    os.system("git push origin main")
